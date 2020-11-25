2ffda6c39285f3fbd7d90555bc2e47d839f8f084
xumamba <Xu_Liangpan@163.com>
2020-11-25 23:19:25 +0800

    Feat: grpc stream example

--- example/grpc/stream/server/main.go
+++ example/grpc/stream/server/main.go
@@ -1,38 +1,101 @@
-package server
+package main
 
-/**
-* @Time       : 2020/11/22 9:07 下午
-* @Author     : xumamba
-* @Description: stream example server
- */
 import (
 	"context"
+	"errors"
+	"io"
+	"log"
+	"net"
+	"sync"
+	"time"
 
-	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc"
 
 	pb "go-life/example/grpc/stream"
 )
 
-type server struct {
-	pb.UnimplementedRouteGuideServer
+type StudentServer struct {
+	pb.UnimplementedStudentsServer
 
-	FeaturesDB []*pb.Feature
+	locker    sync.RWMutex
+	classInfo map[int32][]*pb.Student
 }
 
-func (s *server) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
-	for _, feature := range s.FeaturesDB{
-		if proto.Equal(feature.Location, point){
-			return feature, nil
+func (s *StudentServer) GetStudent(ctx context.Context, student *pb.Student) (*pb.Feature, error) {
+	log.Printf("[GetStudent] receive request: %+v\n", student)
+
+	classID := s.divideClass(student.Id)
+	s.locker.RLock()
+	defer s.locker.RUnlock()
+	for _, body := range s.classInfo[classID] {
+		if body.Id == student.Id {
+			return &pb.Feature{ClassId: classID, StudentInfo: student}, nil
 		}
 	}
-	return &pb.Feature{Location: point}, nil
+	return nil, errors.New("no information about the student was found")
 }
 
-func (s *server) ListFeatures(rectangle *pb.Rectangle, featuresServer pb.RouteGuide_ListFeaturesServer) error {
-	panic("implement me")
+func (s *StudentServer) ListStudents(class *pb.Class, studentsServer pb.Students_ListStudentsServer) error {
+	log.Printf("[ListStudents] receive request: %+v\n", class)
+	s.locker.RLock()
+	defer s.locker.RUnlock()
+	for _, student := range s.classInfo[class.ClassId] {
+		log.Println(student)
+		if err := studentsServer.Send(student); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (s *server) RecordRoute(routeServer pb.RouteGuide_RecordRouteServer) error {
-	panic("implement me")
+func (s *StudentServer) RecordStudents(studentsServer pb.Students_RecordStudentsServer) error {
+	var studentCount, classCount int32
+	startTime := time.Now()
+	s.locker.Lock()
+	defer s.locker.Unlock()
+	for {
+		student, err := studentsServer.Recv()
+		log.Printf("[RecordStudents] receive student: %+v\n", student)
+		if err == io.EOF {
+			endTime := time.Now()
+			return studentsServer.SendAndClose(&pb.StudentSummary{
+				StudentCount: studentCount,
+				ClassCount:   classCount,
+				ElapsedTime:  int32(endTime.Sub(startTime).Microseconds()),
+			})
+		}
+		if err != nil {
+			return err
+		}
+		studentCount++
+		classID := s.divideClass(student.Id)
+		if _, ok := s.classInfo[classID]; !ok {
+			classCount++
+			s.classInfo[classID] = make([]*pb.Student, 0)
+		}
+		s.classInfo[classID] = append(s.classInfo[classID], student)
+	}
 }
 
+func NewServer() *StudentServer {
+	s := &StudentServer{
+		classInfo: make(map[int32][]*pb.Student),
+	}
+	return s
+}
+
+func (s *StudentServer) divideClass(id int64) int32 {
+	return int32(id % 8)
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:50005")
+	if err != nil {
+		log.Fatalf("failed to listen:%v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterStudentsServer(s, NewServer())
+	if err = s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
