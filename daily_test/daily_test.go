f3441a41a98f4316a06ff64838c7344d0d4a8e14
xumamba <Xu_Liangpan@163.com>
2020-12-01 22:15:06 +0800

    Test: select scenario

--- daily_test/daily_test.go
+++ daily_test/daily_test.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type S1 struct {
-	name string
-	age int
+	name  string
+	age   int
 	hobby []string
 }
 
 type S2 struct {
 	name string
-	age int
+	age  int
 }
 
 func TestCompareStruct(t *testing.T) {
@@ -24,4 +26,21 @@ func TestCompareStruct(t *testing.T) {
 	println(s2 == s3) // true
 	s4 := S1{hobby: []string{"read"}}
 	println(reflect.DeepEqual(s1, s4)) // true
+}
+
+func TestSelect(t *testing.T) {
+	var ch = make(chan int)
+	go func() {
+		ch <- 1
+	}()
+	time.Sleep(1 *time.Second)
+	select {
+	case x := <- ch:
+		fmt.Println("receive: ", x)
+	case y := <-time.After(3 * time.Second):
+		fmt.Println("y is: ", y)
+	default:
+		fmt.Println("nothing")
+	}
+	fmt.Println("exit.")
 }
